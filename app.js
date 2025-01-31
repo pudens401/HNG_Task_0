f958f456e3f768981d6d1cd33bebdac6c857fd95
pudens401 <kundapudens@gmail.com>
2025-01-31 18:33:10 +0200

    ~

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ app.use(cors({credentials:true}))
 app.use(bodyParser.json())
 
 
-app.get('/getInfo',(req,res)=>{
+app.get('/',(req,res)=>{
     try {
         const email = "iradukunda401@gmail.com"
         const current_datetime = new Date().toISOString();
-        const 
+        const github_url = "https://github.com/pudens401/HNG_Task_0"
+
         return res.status(200).json({
+            "email":email,
+            "current_datetime": current_datetime,
+            "github_url":github_url
 
         })
     } catch (error) {
-        
+        return res.status(200).json({error:error.message})
     }
+})
+
+const PORT = process.env.PORT?process.env.PORT:8000
+app.listen(PORT,()=>{
+    console.log(`Connected to server on ${PORT}`)
 })
 
 
