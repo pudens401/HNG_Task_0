30d7cc9b281d56ec658cc059440dd605fa69bb31
pudens401 <kundapudens@gmail.com>
2025-01-31 17:47:23 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json({limit:'100mb'}))
+app.use(express.urlencoded({limit:'100mb',extended:true}))
+app.use(cors({credentials:true}))
+app.use(bodyParser.json())
+
+
+app.get('/getInfo',(req,res)=>{
+    try {
+        
+        return res.status(200).json({
+
+        })
+    } catch (error) {
+        
+    }
+})
+
+
